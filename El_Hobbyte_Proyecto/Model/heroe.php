264daef41196af897e301ddf597136459d6f900a
CarlosRamirezSantos <carlosramii2304@gmail.com>
2025-10-06 08:53:59 +0200

    Creación del modelo héroe

---
+++ El_Hobbyte_Proyecto/Model/heroe.php
@@ -0,0 +1,31 @@
+<?php
+
+class Heroe {
+    private $id;
+    private $id_partida;
+    private $nombre;              
+    private $capacidad_maxima;    
+    private $capacidad_actual;    
+    private $estado;      
+
+    public function __construct($id, $id_partida, $nombre, $capacidad_maxima, $capacidad_actual, $estado) {
+        $this->id = $id;
+        $this->id_partida = $id_partida;
+        $this->nombre = $nombre;
+        $this->capacidad_maxima = $capacidad_maxima;
+        $this->capacidad_actual = $capacidad_actual;
+        $this->estado = $estado;
+    }
+
+    
+    public function getId() { return $this->id; }
+    public function getIdPartida() { return $this->id_partida; }
+    public function getNombre() { return $this->nombre; }
+    public function getCapacidadMaxima() { return $this->capacidad_maxima; }
+    public function getCapacidadActual() { return $this->capacidad_actual; }
+    public function getEstado() { return $this->estado; }
+
+ 
+    public function setCapacidadActual($capacidad_actual) { $this->capacidad_actual = $capacidad_actual; }
+    public function setEstado($estado) { $this->estado = $estado; }
+}
